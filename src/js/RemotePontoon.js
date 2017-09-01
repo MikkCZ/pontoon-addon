MikkCZ/pontoon-addon

commit 7ca6ede2db829e3dcc4811d033d94fd5a8db93ee

Store notifications data as object

src/js/RemotePontoon.js
@@ -3,6 +3,7 @@ function RemotePontoon(team) {
     this._notificationsUrl = this._baseUrl + '/notifications/';
     this._markAsReadUrl = this._notificationsUrl + 'mark-all-as-read/';
     this._team = team;
+    this._domParser = new DOMParser();
     this._listenToMessagesFromContent();
     this._watchOptionsUpdates();
 }
@@ -40,7 +41,25 @@ RemotePontoon.prototype = {
         return `${this._baseUrl}/${this._team}/mozillaorg/all-resources/?search=${textToSearch.replace(' ', '+')}`;
     },
 
-    updateNotificationsDocText: function() {
+    _updateNotificationsDataFromPageContent: function(notificationsPageContent) {
+        if (notificationsPageContent) {
+            var notificationsDataObj = {};
+            var notificationsPage = this._domParser.parseFromString(notificationsPageContent, 'text/html');
+            for (const n of notificationsPage.querySelectorAll('header .notification-item[data-unread=true]')) {
+                var nObj = {};
+                nObj.id = n.dataset.id;
+                nObj.link = {text: n.getElementsByTagName('a')[0].textContent, href: n.getElementsByTagName('a')[0].getAttribute('href')};
+                nObj.description = n.querySelectorAll('.verb')[0].textContent;
+                nObj.timeago = n.querySelectorAll('.timeago')[0].textContent;
+                notificationsDataObj[n.dataset.id] = nObj;
+            }
+            chrome.storage.local.set({notificationsData: notificationsDataObj});
+        } else {
+            chrome.storage.local.set({notificationsData: undefined});
+        }
+    },
+
+    updateNotificationsData: function() {
         fetch(this.getNotificationsUrl(), {
             credentials: 'include',
             redirect: 'manual',
@@ -51,11 +70,7 @@ RemotePontoon.prototype = {
                 return undefined;
             }
         }.bind(this)).then(function(text) {
-            if (text != undefined) {
-                chrome.storage.local.set({notificationsDocText: text});
-            } else {
-                chrome.storage.local.set({notificationsDocText: undefined});
-            }
+            this._updateNotificationsDataFromPageContent(text);
         }.bind(this));
     },
 
@@ -63,7 +78,7 @@ RemotePontoon.prototype = {
         chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
             switch (request.type) {
                 case 'pontoon-page-loaded':
-                    chrome.storage.local.set({notificationsDocText: request.value});
+                    this._updateNotificationsDataFromPageContent(request.value);
                     break;
                 case 'mark-all-notifications-as-read-from-page':
                     this.markAllNotificationsAsRead();

src/js/NotificationsPopup.js
@@ -12,19 +12,18 @@ NotificationsPopup.prototype = {
 
     _watchStorageChanges: function() {
         chrome.storage.onChanged.addListener(function(changes, areaName) {
-            if (changes['notificationsDocText'] !== undefined) {
-                this._loadNotificationsFromDocContent(changes['notificationsDocText'].newValue);
+            if (changes['nofiticationsData'] !== undefined) {
+                this._displayNotifications(changes['nofiticationsData'].newValue);
             }
         }.bind(this));
     },
 
     _appendNotificationToList: function(list, notification) {
-        var sourceLink = notification.getElementsByTagName('a')[0];
         var link = document.createElement('a');
-        link.textContent = sourceLink.textContent;
-        link.setAttribute('href', this._remotePontoon.getTeamProjectUrl(sourceLink.getAttribute('href')));
+        link.textContent = notification.link.text;
+        link.setAttribute('href', this._remotePontoon.getTeamProjectUrl(notification.link.href));
         var description = document.createElement('span');
-        description.textContent = notification.querySelectorAll('.verb')[0].textContent + ' ' + notification.querySelectorAll('.timeago')[0].textContent;
+        description.textContent = notification.description + ' ' + notification.timeago;
         var listItem = document.createElement('li');
         listItem.appendChild(link);
         listItem.appendChild(document.createElement('br'));
@@ -32,24 +31,24 @@ NotificationsPopup.prototype = {
         list.appendChild(listItem);
     },
 
-    _displayNotifications: function(notifications) {
+    _displayNotifications: function(notificationsData) {
         var notificationsList = document.getElementById('notification-list');
         var fullList = document.getElementById('full-list');
         var emptyList = document.getElementById('empty-list');
         var error = document.getElementById('error');
 
-        if (notifications != undefined && notifications.length > 0) {
+        if (notificationsData != undefined && Object.keys(notificationsData).length > 0) {
             while (notificationsList.lastChild) {
                 notificationsList.removeChild(notificationsList.lastChild);
             }
-            for (const n of notifications) {
-                this._appendNotificationToList(notificationsList, n);
+            for (const nKey of Object.keys(notificationsData).sort().reverse()) {
+                this._appendNotificationToList(notificationsList, notificationsData[nKey]);
             }
             notificationsList.classList.remove('hidden');
             fullList.classList.remove('hidden');
             emptyList.classList.add('hidden');
             error.classList.add('hidden');
-        } else if (notifications != undefined) {
+        } else if (notificationsData != undefined) {
             notificationsList.classList.add('hidden');
             fullList.classList.add('hidden');
             emptyList.classList.remove('hidden');
@@ -62,24 +61,10 @@ NotificationsPopup.prototype = {
         }
     },
 
-    _loadNotificationsFromDocContent: function(docContent) {
-        if (docContent != undefined) {
-            var notificationsDoc = new DOMParser().parseFromString(docContent, 'text/html');
-            var unreadNotifications = notificationsDoc.querySelectorAll('header .notification-item[data-unread=true]');
-            this._displayNotifications(unreadNotifications);
-        } else {
-            this._displayNotifications(undefined);
-        }
-    },
-
     _loadNotificationsFromStorage: function() {
-        var docKey = 'notificationsDocText';
-        chrome.storage.local.get(docKey, function(item) {
-            if (item[docKey] != undefined) {
-                this._loadNotificationsFromDocContent(item[docKey]);
-            } else {
-                this._loadNotificationsFromDocContent(undefined)
-            }
+        var dataKey = 'notificationsData';
+        chrome.storage.local.get(dataKey, function(item) {
+            this._displayNotifications(item[dataKey]);
         }.bind(this));
     },
 }

src/js/ToolbarButton.js
@@ -21,7 +21,7 @@ ToolbarButton.prototype = {
 
     _updateNumberOfUnreadNotifications: function() {
         this._updateBadge('');
-        this._remotePontoon.updateNotificationsDocText();
+        this._remotePontoon.updateNotificationsData();
     },
 
     _setRefresh: function(intervalMinutes) {
@@ -47,13 +47,11 @@ ToolbarButton.prototype = {
 
     _watchStorageChanges: function() {
         chrome.storage.onChanged.addListener(function(changes, areaName) {
-            var docKey = 'notificationsDocText';
-            if (changes[docKey] !== undefined) {
-                var docContent = changes[docKey].newValue;
-                if (docContent != undefined) {
-                    var notificationsDoc = new DOMParser().parseFromString(docContent, 'text/html');
-                    var unreadCount = notificationsDoc.querySelectorAll('header .notification-item[data-unread=true]').length;
-                    this._updateBadge(unreadCount);
+            var dataKey = 'notificationsData';
+            if (changes[dataKey] !== undefined) {
+                var notificationsData = changes[dataKey].newValue;
+                if (notificationsData != undefined) {
+                    this._updateBadge(Object.keys(notificationsData).length);
                 } else {
                     this._updateBadge('!');
                     this._scheduleOrUpdateRefresh();

commit 5530d4da583345e78401adbd93ea0e2e4546b227

Fix login detection
regression from 7ca6ede2db829e3dcc4811d033d94fd5a8db93ee

src/js/RemotePontoon.js
@@ -42,9 +42,9 @@ RemotePontoon.prototype = {
     },
 
     _updateNotificationsDataFromPageContent: function(notificationsPageContent) {
-        if (notificationsPageContent) {
+        var notificationsPage = this._domParser.parseFromString(notificationsPageContent, 'text/html');
+        if (notificationsPage.querySelectorAll('header #notifications').length > 0) {
             var notificationsDataObj = {};
-            var notificationsPage = this._domParser.parseFromString(notificationsPageContent, 'text/html');
             for (const n of notificationsPage.querySelectorAll('header .notification-item[data-unread=true]')) {
                 var nObj = {};
                 nObj.id = n.dataset.id;
@@ -62,13 +62,8 @@ RemotePontoon.prototype = {
     updateNotificationsData: function() {
         fetch(this.getNotificationsUrl(), {
             credentials: 'include',
-            redirect: 'manual',
         }).then(function(response) {
-            if (response.status == 200) {
-                return response.text();
-            } else {
-                return undefined;
-            }
+            return response.text();
         }.bind(this)).then(function(text) {
             this._updateNotificationsDataFromPageContent(text);
         }.bind(this));