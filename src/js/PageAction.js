0ad464b3a45d930df3b701fc7589e719a0b7a218
Michal Stanke <michal@stanke.cz>
2017-09-13 09:01:00 +0200

    Show page action title with the project name, #20

--- src/js/PageAction.js
+++ src/js/PageAction.js
@@ -16,7 +16,7 @@ class PageAction {
      */
     _addClickAction() {
         chrome.pageAction.onClicked.addListener((tab) =>
-            chrome.tabs.create({url: this._remotePontoon.getTeamProjectUrl(this._getPontoonProjectUrlForPageUrl(tab.url))+'all-resources/'})
+            chrome.tabs.create({url: this._getPontoonProjectUrlForPageUrl(tab.url)})
         );
     }
 
@@ -26,9 +26,12 @@ class PageAction {
      */
     _watchTabsUpdates() {
         chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-            if (changeInfo.status === 'complete' && this._getPontoonProjectUrlForPageUrl(tab.url)) {
-                chrome.pageAction.show(tabId);
-                // TODO: set page action title "Open ${project.name} in Pontoon"
+            if (changeInfo.status === 'complete') {
+                const projectData = this._getPontoonProjectForPageUrl(tab.url);
+                if (projectData) {
+                    chrome.pageAction.setTitle({tabId: tabId, title: `Open ${projectData.name} in Pontoon`});
+                    chrome.pageAction.show(tabId);
+                }
             }
         });
     }
@@ -40,31 +43,41 @@ class PageAction {
      * @private
      */
     _getPontoonProjectUrlForPageUrl(pageUrl) {
+        const projectData = this._getPontoonProjectForPageUrl(pageUrl);
+        if (projectData) {
+            return this._remotePontoon.getTeamProjectUrl(`/projects/${projectData.slug}/all-resources/`);
+        } else {
+            return undefined;
+        }
+    }
+
+    /**
+     * Get Pontoon project for the loaded page.
+     * @param pageUrl loaded in a tab
+     * @returns {object|undefined}
+     * @private
+     */
+    _getPontoonProjectForPageUrl(pageUrl) {
         const tmpLink = document.createElement('a');
         tmpLink.href = pageUrl;
         // TODO: refactor this elsewhere
         const toProjectMap = new Map([
-            ['addons.mozilla.org', 'amo'],
-            ['www.changecopyright.org', 'copyright-campaign'],
-            ['accounts.firefox.com', 'firefox-accounts'],
-            ['screenshots.firefox.com', 'firefox-screenshots'],
-            ['donate.mozilla.org', 'fundraising'],
-            ['developer.mozilla.org', 'mdn'],
-            ['advocacy.mozilla.org', 'mozilla-advocacy'],
-            ['learning.mozilla.org', 'mozilla-learning-network'],
-            ['www.mozilla.org', 'mozillaorg'],
-            ['www-dev.allizom.org', 'mozillaorg'],
-            ['mozillians.org', 'mozillians'],
-            ['support.mozilla.org', 'sumo'],
-            ['send.firefox.com', 'test-pilot-firefox-send'],
-            ['testpilot.firefox.com', 'test-pilot-website'],
-            ['thimble.mozilla.org', 'thimble']
+            ['addons.mozilla.org', {slug: 'amo', name: 'AMO'}],
+            ['www.changecopyright.org', {slug: 'copyright-campaign', name: 'EU Copyright campaign'}],
+            ['accounts.firefox.com', {slug: 'firefox-accounts', name: 'Firefox Accounts'}],
+            ['screenshots.firefox.com', {slug: 'firefox-screenshots', name: 'Firefox Screenshots'}],
+            ['donate.mozilla.org', {slug: 'fundraising', name: 'Fundraising'}],
+            ['developer.mozilla.org', {slug: 'mdn', name: 'MDN'}],
+            ['advocacy.mozilla.org', {slug: 'mozilla-advocacy', name: 'Mozilla Advocacy'}],
+            ['learning.mozilla.org', {slug: 'mozilla-learning-network', name: 'Mozilla Learning Network'}],
+            ['www.mozilla.org', {slug: 'mozillaorg', name: 'Mozilla.org'}],
+            ['www-dev.allizom.org', {slug: 'mozillaorg', name: 'Mozilla.org'}],
+            ['mozillians.org', {slug: 'mozillians', name: 'Mozillians'}],
+            ['support.mozilla.org', {slug: 'sumo', name: 'SUMO'}],
+            ['send.firefox.com', {slug: 'test-pilot-firefox-send', name: 'Test Pilot: Firefox Send'}],
+            ['testpilot.firefox.com', {slug: 'test-pilot-website', name: 'Test Pilot: Website'}],
+            ['thimble.mozilla.org', {slug: 'thimble', name: 'Thimble'}]
         ]);
-        const project = toProjectMap.get(tmpLink.hostname);
-        if (project) {
-            return `/projects/${project}/`;
-        } else {
-            return undefined;
-        }
+        return toProjectMap.get(tmpLink.hostname);
     }
 }
