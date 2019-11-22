3fa7890f23e96d73955d36b18cd6eb8a23cfa1aa
Michal Stanke <michal@stanke.cz>
2019-11-22 18:37:35 +0100

    Test WebExtensions API interactions with sinon-chrome, fix #66
    - more test will be added when rewriting the code to React etc.

--- src/packages/intro/src/CloseButton.test.js
+++ src/packages/intro/src/CloseButton.test.js
@@ -1,8 +1,13 @@
+/* global browser, flushPromises */
 import React from 'react';
 import { mount } from 'enzyme';
 import { CloseButton } from './CloseButton';
 
 describe('<CloseButton>', () => {
+
+  afterEach(() => {
+    browser.flush();
+  });
 
   it('has close icon', () => {
     const wrapper = mount(
@@ -18,6 +23,20 @@ describe('<CloseButton>', () => {
     );
 
     expect(wrapper.find('button').getDOMNode()).toHaveStyle('border: 1px solid black');
+  });
+
+  it('closes intro page', async () => {
+    const wrapper = mount(
+      <CloseButton />
+    );
+
+    const fakeTab = {id: 'fake-tab_id'};
+    browser.tabs.query.withArgs({currentWindow: true, active: true}).resolves([fakeTab]);
+
+    wrapper.find('button').simulate('click');
+    await flushPromises();
+
+    expect(browser.tabs.remove.withArgs(fakeTab.id).calledOnce).toBe(true);
   });
 
 });
--- src/packages/intro/src/setupTests.js
+++ src/packages/intro/src/setupTests.js
@@ -1,7 +1,12 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import '@testing-library/jest-dom/extend-expect';
+import browser from 'sinon-chrome/webextensions';
 
 configure({ adapter: new Adapter() });
 
-global.browser = {};
+global.browser = browser;
+
+global.flushPromises = () => {
+  return new Promise((resolve) => setImmediate(resolve));
+}
