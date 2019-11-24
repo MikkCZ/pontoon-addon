0bbbf09a1995fc755b2e0f2abfd6381ea0e560e8
Michal Stanke <michal@stanke.cz>
2019-11-24 13:30:49 +0100

    Fix Code Climate issue

--- src/packages/intro/src/index.test.js
+++ src/packages/intro/src/index.test.js
@@ -5,7 +5,7 @@ describe('index.js', () => {
 
   it('renders', async () => {
     const optionsStub = sinon.stub(optionsModule, 'Options');
-    sinon.stub(optionsModule.Options, 'create').callsFake(async () => {});
+    sinon.stub(optionsModule.Options, 'create').callsFake(async () => optionsStub);
 
     const reactDomRender = await require('./index.js');
     const panelSection = await reactDomRender.default;
