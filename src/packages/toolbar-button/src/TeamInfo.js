8992b955249f475ae9bd808477f104cca37bd04e
Michal Stanke <michal@stanke.cz>
2019-11-26 19:37:23 +0100

    Display team information

--- src/packages/toolbar-button/src/TeamInfo.js
+++ src/packages/toolbar-button/src/TeamInfo.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import ReactTimeAgo from 'react-time-ago';
+import { TeamInfoListItem } from './TeamInfoListItem';
 import { BottomLink } from './BottomLink';
+import lightbulbIcon from './static/img/lightbulb-blue.svg'
 import 'Commons/static/css/pontoon.css';
 import './TeamInfo.css';
 if (!browser) { // eslint-disable-line no-use-before-define
@@ -16,6 +19,11 @@ export class TeamInfo extends React.Component {
     browser.tabs.create({url: teamPageUrl}).then(() => window.close());
   }
 
+  async _openTeamStringsWithStatus(status) {
+    const searchUrl = await this.props.backgroundPontoonClient.getStringsWithStatusSearchUrl(status);
+    browser.tabs.create({url: searchUrl}).then(() => window.close());
+  }
+
   render() {
     return (
       <section className="TeamInfo">
@@ -25,7 +33,37 @@ export class TeamInfo extends React.Component {
           </button>
         </h1>
         <ul className="TeamInfo-list">
-
+          {
+            this.props.latestActivity &&
+              <TeamInfoListItem
+                key="activity"
+                label="Activity"
+                value={
+                  <React.Fragment>
+                    {this.props.latestActivity.user} <ReactTimeAgo date={ new Date(this.props.latestActivity.date_iso) } />
+                  </React.Fragment>
+                }
+              />
+          }
+          {
+            [
+              {status: 'translated', text: 'translated', dataProperty: 'approvedStrings', labelBeforeStyle: {backgroundColor:'#7bc876'}},
+              {status: 'fuzzy', text: 'fuzzy', dataProperty: 'fuzzyStrings', labelBeforeStyle: {backgroundColor: '#fed271'}},
+              {status: 'warnings', text: 'warnings', dataProperty: 'stringsWithWarnings', labelBeforeStyle: {backgroundColor:'#ffa10f'}},
+              {status: 'errors', text: 'errors', dataProperty: 'stringsWithErrors', labelBeforeStyle: {backgroundColor:'#f36'}},
+              {status: 'missing', text: 'missing', dataProperty: 'missingStrings', labelBeforeStyle: {backgroundColor:'#4d5967'}},
+              {status: 'unreviewed', text: 'unreviewed', dataProperty: 'unreviewedStrings', labelBeforeStyle: {height:'1em', background:`center / contain no-repeat url(${lightbulbIcon})`}},
+              {status: 'all', text: 'all strings', dataProperty: 'totalStrings'}
+            ].map((category) =>
+              <TeamInfoListItem
+                key={category.status}
+                labelBeforeStyle={category.labelBeforeStyle}
+                label={category.text}
+                value={this.props.stringsData[category.dataProperty]}
+                onClick={() => this._openTeamStringsWithStatus(category.status)}
+              />
+            )
+          }
         </ul>
         <BottomLink
           className="TeamInfo-team-page"
--- src/packages/toolbar-button/src/index.js
+++ src/packages/toolbar-button/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import JavascriptTimeAgo from 'javascript-time-ago';
+import en from 'javascript-time-ago/locale/en';
 import { Options } from 'Commons/src/Options';
 import { BackgroundPontoonClient } from 'Commons/src/BackgroundPontoonClient';
 import { NotificationsList } from './NotificationsList';
@@ -17,23 +19,42 @@ if (!browser) { // eslint-disable-line no-use-before-define
  */
 
 export default Options.create().then(async (options) => {
-    const backgroundPontoonClient = new BackgroundPontoonClient();
+  JavascriptTimeAgo.locale(en);
+  const backgroundPontoonClient = new BackgroundPontoonClient();
 
-    return ReactDOM.render(
-      <React.Fragment>
-        <NotificationsListError
-          backgroundPontoonClient={backgroundPontoonClient}
-        />
-        <NotificationsList
-          notificationsData={[]}
-          backgroundPontoonClient={backgroundPontoonClient}
-        />
-        <TeamInfo
-          name='NAME'
-          code='co-de'
-          backgroundPontoonClient={backgroundPontoonClient}
-        />
-      </React.Fragment>,
-      document.getElementById('root') || document.createElement('div')
-    );
+  const localeTeamOptionKey = 'locale_team';
+  const teamsListKey = 'teamsList';
+  const latestTeamsActivityKey = 'latestTeamsActivity';
+  const [teamData, latestTeamActivity] = await Promise.all([
+    options.get(localeTeamOptionKey).then((item) => item[localeTeamOptionKey]),
+    browser.storage.local.get(teamsListKey).then((item) => item[teamsListKey]),
+    browser.storage.local.get(latestTeamsActivityKey).then((item) => item[latestTeamsActivityKey]),
+  ]).then(([
+    localeTeam,
+    teamsList,
+    latestTeamsActivity,
+  ]) => [
+    teamsList[localeTeam],
+    latestTeamsActivity[localeTeam],
+  ]);
+
+  return ReactDOM.render(
+    <React.Fragment>
+      <NotificationsListError
+        backgroundPontoonClient={backgroundPontoonClient}
+      />
+      <NotificationsList
+        notificationsData={[]}
+        backgroundPontoonClient={backgroundPontoonClient}
+      />
+      <TeamInfo
+        name={teamData.name}
+        code={teamData.code}
+        stringsData={teamData.strings}
+        latestActivity={latestTeamActivity}
+        backgroundPontoonClient={backgroundPontoonClient}
+      />
+    </React.Fragment>,
+    document.getElementById('root') || document.createElement('div')
+  );
 });
