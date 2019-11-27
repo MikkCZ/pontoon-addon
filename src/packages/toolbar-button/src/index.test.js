2012377a88773f0769e9267509dd3822eb1a5cd5
Michal Stanke <michal@stanke.cz>
2019-11-27 12:01:57 +0100

    Test toolbar-button index

---
+++ src/packages/toolbar-button/src/index.test.js
@@ -0,0 +1,35 @@
+/* global browser */
+import sinon from 'sinon';
+import * as optionsModule from 'Commons/src/Options';
+
+describe('index.js', () => {
+
+  afterEach(() => {
+    browser.flush();
+  });
+
+  it('renders', async () => {
+    const optionsStub = sinon.stub(optionsModule, 'Options');
+    optionsStub.get = sinon.stub(optionsStub, 'get');
+    sinon.stub(optionsModule.Options, 'create').callsFake(async () => optionsStub);
+    optionsStub.get.resolves({
+      toolbar_button_popup_always_hide_read_notifications: false,
+      locale_team: 'cs',
+    });
+
+    browser.storage.local.get.resolves({
+      notificationsData: {},
+      teamsList: {
+        cs: { name: 'Czech', code: 'cs', strings: {} },
+      },
+      latestTeamsActivity: {},
+    })
+
+    const reactDomRender = await require('./index.js');
+    const rootFragment = await reactDomRender.default;
+
+    expect(rootFragment.constructor.name).toBe('NotificationsList');
+    // TODO: check TeamInfo too
+  });
+
+});
