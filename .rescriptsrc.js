62ce04db9809d8432f20b00f02859e665677a55f
Michal Stanke <michal@stanke.cz>
2020-03-12 17:03:08 +0100

    Remove hashes and service worker to ensure 100 % repeatable build

--- .rescriptsrc.js
+++ .rescriptsrc.js
@@ -1,11 +1,28 @@
-const path = require('path');
+'use strict';
 
-module.exports = (config) => {
-  config.mode = 'production';
-  config.devtool = 'cheap-source-map';
-  config.resolve.plugins = config.resolve.plugins.filter((plugin) =>
-    plugin.constructor.name !== 'ModuleScopePlugin'
-  );
-  config.resolve.alias.Commons = path.resolve(__dirname, 'src/packages/commons/');
-  return config;
-};
+const path = require('path');
+const noHashesPlugin = require('react-scripts-plugin-no-hashes');
+
+class PontoonAddonRescripts {
+  static modifyConfig(config) {
+    config.mode = 'production';
+    config.devtool = 'cheap-source-map';
+    config.plugins = config.plugins.filter((plugin) =>
+      PontoonAddonRescripts.isPluginToKeep(plugin.constructor.name)
+    );
+    config.resolve.plugins = config.resolve.plugins.filter((plugin) =>
+      PontoonAddonRescripts.isPluginToKeep(plugin.constructor.name)
+    );
+    config.resolve.alias.Commons = path.resolve(__dirname, 'src/packages/commons/');
+    config = noHashesPlugin.apply(config, {env: config.mode});
+    return config;
+  }
+
+  static isPluginToKeep(pluginConstructorName) {
+    return ![
+      'ModuleScopePlugin', 'GenerateSW'
+    ].some((toRemove) => pluginConstructorName.toUpperCase() === toRemove.toUpperCase());
+  }
+}
+
+module.exports = PontoonAddonRescripts.modifyConfig;
