9984b1b43bd9f2d85d63ade048f97722d893acf1
Michal Stanke <michal@stanke.cz>
2021-03-15 08:50:47 +0100

    Minimize webpack stdout

--- src/packages/content-scripts/webpack.config.js
+++ src/packages/content-scripts/webpack.config.js
@@ -3,6 +3,10 @@ const path = require('path');
 module.exports = {
   mode: 'production',
   devtool: 'cheap-source-map',
+  performance: {
+    hints: false,
+  },
+  stats: 'errors-only',
   resolve: {
     alias: {
       '@pontoon-addon/commons': path.resolve(__dirname, '../commons/'),
