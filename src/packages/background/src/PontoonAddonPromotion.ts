d0841561e3bd878644f0918e20a6134856ce5768
Michal Stanke <michal@stanke.cz>
2021-04-29 17:57:01 +0200

    Register promotion script faster with contentScripts API

--- src/packages/background/src/PontoonAddonPromotion.ts
+++ src/packages/background/src/PontoonAddonPromotion.ts
@@ -1,47 +1,41 @@
-import type { ContentScripts, Tabs } from 'webextension-polyfill-ts';
+import type { ContentScripts } from 'webextension-polyfill-ts';
 
 import type { RemotePontoon } from './RemotePontoon';
 import { browser } from './util/webExtensionsApi';
 
 export class PontoonAddonPromotion {
-  private readonly _remotePontoon: RemotePontoon;
-  private _contentScript: ContentScripts.RegisteredContentScript | undefined;
+  private readonly remotePontoon: RemotePontoon;
+  private contentScript: ContentScripts.RegisteredContentScript | undefined;
 
   constructor(remotePontoon: RemotePontoon) {
-    this._remotePontoon = remotePontoon;
-    this._remotePontoon.subscribeToBaseUrlChange(() =>
-      this._injectContentScriptIntoAllPontoonTabs()
-    );
-    this._watchTabUpdates();
-    this._injectContentScriptIntoAllPontoonTabs();
-  }
-
-  private _watchTabUpdates(): void {
-    browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-      if (changeInfo.status === 'complete' && this._isPontoonServer(tab.url)) {
-        this._injectContentScript(tab);
-      }
+    this.remotePontoon = remotePontoon;
+    this.remotePontoon.subscribeToBaseUrlChange(() => {
+      this.registerContentScript();
     });
+    this.registerContentScript();
   }
 
-  private async _injectContentScriptIntoAllPontoonTabs(): Promise<void> {
+  private async registerContentScript(): Promise<void> {
+    const contentScriptInfo = {
+      file:
+        'packages/content-scripts/dist/pontoon-addon-promotion-content-script.js',
+    };
+    await this.contentScript?.unregister();
+    this.contentScript = await browser.contentScripts.register({
+      js: [contentScriptInfo],
+      matches: [`${this.remotePontoon.getBaseUrl()}/*`],
+      runAt: 'document_end', // Corresponds to interactive. The DOM has finished loading, but resources such as scripts and images may still be loading.
+    });
     (await browser.tabs.query({}))
-      .filter((tab) => this._isPontoonServer(tab.url))
-      .forEach((tab) => this._injectContentScript(tab));
+      .filter((tab) => this.isPontoonServer(tab.url))
+      .forEach((tab) => browser.tabs.executeScript(tab.id, contentScriptInfo));
   }
 
-  private _isPontoonServer(url: string | undefined): boolean {
+  private isPontoonServer(url: string | undefined): boolean {
     if (url) {
-      return url.startsWith(`${this._remotePontoon.getBaseUrl()}/`);
+      return url.startsWith(`${this.remotePontoon.getBaseUrl()}/`);
     } else {
       return false;
     }
-  }
-
-  private _injectContentScript(tab: Tabs.Tab): void {
-    browser.tabs.executeScript(tab.id, {
-      file:
-        'packages/content-scripts/dist/pontoon-addon-promotion-content-script.js',
-    });
   }
 }
