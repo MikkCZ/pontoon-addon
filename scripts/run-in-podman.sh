9cbd54bc5934109e58f47a7d165158521e507d32
Michal Stanke <michal@stanke.cz>
2022-03-10 20:00:14 +0100

    All GitHub workflows in containers

--- scripts/run-in-podman.sh
+++ scripts/run-in-podman.sh
@@ -3,15 +3,15 @@
 set -e
 set -o pipefail
 
-CONTAINER_IMAGE='docker.io/library/node:14-buster' # LTSs
+CONTAINER_IMAGE='docker.io/library/node:14-buster' # LTS
 COMMAND=${@}
 
 podman pull "${CONTAINER_IMAGE}"
 podman run \
+  -e CODECOV_TOKEN="${CODECOV_TOKEN}" \
   --workdir "${PWD}" \
   -v "${PWD}:${PWD}:Z" \
   -v "${PWD}/scripts/.yarnrc:${PWD}/.yarnrc:Z" \
   --rm=true \
   --entrypoint=/bin/bash \
-  "${CONTAINER_IMAGE}" -c "${COMMAND}"  \
-  -e CODECOV_TOKEN = $CODECOV_TOKEN 
+  "${CONTAINER_IMAGE}" -c "${COMMAND}"
