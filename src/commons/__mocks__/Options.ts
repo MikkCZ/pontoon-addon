3f11162cc66c0f7a6c5e0572b75e211bddb9655c
Michal Stanke <michal@stanke.cz>
2022-04-28 12:23:33 +0200

    Fix eslint

--- src/commons/__mocks__/Options.ts
+++ src/commons/__mocks__/Options.ts
@@ -5,8 +5,11 @@ import type { OptionId, OptionValue } from '../Options';
 export class Options {
   set: (id: string, value: OptionValue | undefined) => void;
   get: (id: OptionId | OptionId[]) => Promise<OptionValue>;
-  subscribeToOptionChange: (optionId: string, callback: (change: Storage.StorageChange) => void) => void;
-  resetDefaults: () => Promise<void>
+  subscribeToOptionChange: (
+    optionId: string,
+    callback: (change: Storage.StorageChange) => void,
+  ) => void;
+  resetDefaults: () => Promise<void>;
 
   constructor() {
     this.set = jest.fn();
