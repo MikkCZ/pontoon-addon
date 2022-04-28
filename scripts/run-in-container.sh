2e58311f7b1b0ea2003caa85c57b6530ca2a9241
Michal Stanke <michal@stanke.cz>
2022-04-28 13:08:40 +0200

    Unify podman/docker scripts (#196)

--- scripts/run-in-container.sh
+++ scripts/run-in-container.sh
@@ -6,13 +6,19 @@ set -o pipefail
 CONTAINER_IMAGE='docker.io/library/node:16-bullseye' # LTS
 COMMAND=${@}
 
-docker pull "${CONTAINER_IMAGE}"
-docker run --user "$(id -u):$(id -g)" \
+CONTAINER_RUN='podman run'
+
+command -v podman > /dev/null 2>&1 || {
+  CONTAINER_RUN="docker run --user $(id -u):$(id -g)"
+}
+
+${CONTAINER_RUN} \
   -e MODE="${MODE}" \
   -e CODECOV_TOKEN="${CODECOV_TOKEN}" \
   --workdir "${PWD}" \
-  -v "${PWD}:${PWD}" \
-  -v "${PWD}/scripts/.npmrc:${PWD}/.npmrc" \
+  -v "${PWD}:${PWD}:Z" \
+  -v "${PWD}/scripts/.npmrc:${PWD}/.npmrc:Z" \
+  --pull=always \
   --rm=true \
   --entrypoint=/bin/bash \
   "${CONTAINER_IMAGE}" -c "${COMMAND}"
