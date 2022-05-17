e2c84faf3543c76b9450497311e6a82aee3efa94
Michal Stanke <michal@stanke.cz>
2022-05-17 15:09:43 +0200

    Update screenshots and images in the introduction tour

--- src/frontend/intro/App/index.tsx
+++ src/frontend/intro/App/index.tsx
@@ -13,9 +13,10 @@ import { pontoonAddonWiki, mozillaOrg } from '@commons/webLinks';
 import toolbarButtonImage from '@assets/img/toolbar-button.png';
 import notificationsImage from '@assets/img/desktop-notification.svg';
 import pontoonLogo from '@assets/img/pontoon-logo.svg';
-import pageActionImage from '@assets/img/page-action.png';
+import addressBarImage from '@assets/img/address-bar.png';
+import contextMenuImage from '@assets/img/context-menu.png';
 import contextButtonsImage from '@assets/img/context-buttons.png';
-import settingsImage from '@assets/img/settings.png';
+import settingsImage from '@assets/img/settings.svg';
 import feedbackImage from '@assets/img/2-Lions.png';
 
 import { GlobalPontoonStyle } from '../../GlobalPontoonStyle';
@@ -35,101 +36,126 @@ const pageTitle = 'Welcome to Pontoon Add-on';
 export const App: React.FC = () => {
   document.title = pageTitle;
 
-  const tiles: TileProps[] = [];
-  tiles.push({
-    title: 'Pontoon button',
-    imageSrc: toolbarButtonImage,
+  const addressBarTile: TileProps = {
+    title: 'Address bar button',
+    imageSrc: addressBarImage,
     text: (
       <>
-        Pontoon icon in the toolbar offers{' '}
-        <strong>quick access to notifications</strong> and your localization
-        team progress.
-      </>
-    ),
-    button: {
-      text: 'See the Pontoon button',
-      onClick: () => openToolbarButtonPopup(),
-    },
-  });
-  tiles.push({
-    title: 'System notifications',
-    imageSrc: notificationsImage,
-    text: 'Pontoon Add-on can bring notifications directly into your system. Try it!',
-    button: {
-      text: 'Preview system notifications',
-      onClick: () => {
-        setOption('show_notifications', true);
-        createNotification({
-          type: 'basic',
-          iconUrl: pontoonLogo,
-          title: 'Notification from Pontoon',
-          message:
-            'Similar notifications will appear if there is something new in Pontoon. You can click them to open related project in Pontoon.',
-        });
-      },
-    },
-  });
-  if (supportsAddressBar()) {
-    tiles.push({
-      title: 'Address bar button',
-      imageSrc: pageActionImage,
-      text: (
-        <>
-          So called <em>page action</em> is a button in the address bar, that
-          lets you <strong>open in Pontoon the project</strong>, which is
-          related to the current page.
-        </>
-      ),
-      button: {
-        text: 'Try it on mozilla.org',
-        onClick: () => openNewTab(mozillaOrg()),
-      },
-    });
-  }
-  tiles.push({
-    title: 'Context buttons',
-    imageSrc: contextButtonsImage,
-    text: (
-      <>
-        <strong>Look up strings in Pontoon</strong> or{' '}
-        <strong>report bugs</strong> on localizer version of Mozilla website.
-        Just select the text and click the context button.
+        Pontoon <strong>icon in the address bar</strong> indicates if the
+        current website is localized in Pontoon. Click it for quick access to
+        the <strong>website localization</strong>.
       </>
     ),
     button: {
       text: 'Try it on mozilla.org',
       onClick: () => openNewTab(mozillaOrg()),
     },
-  });
-  tiles.push({
-    title: 'Add-on settings',
-    imageSrc: settingsImage,
+  };
+
+  const contextButtonsTile: TileProps = {
+    title: 'Context buttons',
+    imageSrc: contextButtonsImage,
     text: (
       <>
-        Pontoon button, notifications, ... all these features are{' '}
-        <strong>configurable</strong>.
+        On a website localized in Pontoon, select any text to{' '}
+        <strong>find it quickly in Pontoon</strong> or{' '}
+        <strong>propose an improvement</strong>.
       </>
     ),
     button: {
-      text: 'Open Pontoon Add-on settings',
-      onClick: () => openOptions(),
+      text: 'Try it on mozilla.org',
+      onClick: () => openNewTab(mozillaOrg()),
     },
-  });
-  tiles.push({
-    title: 'Feedback and more',
-    imageSrc: feedbackImage,
+  };
+
+  const contextMenuTile: TileProps = {
+    title: 'Context menu',
+    imageSrc: contextMenuImage,
     text: (
       <>
-        This add-on won&apos;t exist and improve without you - Mozilla
-        localizers. Please <strong>share your feedback</strong> and feel free to
-        request new features.
+        On a website localized in Pontoon, right-click anywhere for quick access
+        to the <strong>website localization</strong>.
       </>
     ),
     button: {
-      text: 'Check the wiki',
-      onClick: () => openNewTab(pontoonAddonWiki()),
+      text: 'Try it on mozilla.org',
+      onClick: () => openNewTab(mozillaOrg()),
     },
-  });
+  };
+
+  const tiles: TileProps[] = [
+    {
+      title: 'Pontoon button',
+      imageSrc: toolbarButtonImage,
+      text: (
+        <>
+          Pontoon icon in the toolbar offers quick access to{' '}
+          <strong>notifications</strong>, your <strong>team progress</strong>{' '}
+          and the open <strong>website localization</strong>.
+        </>
+      ),
+      button: {
+        text: 'Preview the Pontoon button',
+        onClick: () => openToolbarButtonPopup(),
+      },
+    },
+    {
+      title: 'System notifications',
+      imageSrc: notificationsImage,
+      text: (
+        <>
+          This add-on brings{' '}
+          <strong>updates from Pontoon one click away</strong>. Try how
+          notifications look directly in your system!
+        </>
+      ),
+      button: {
+        text: 'Preview system notifications',
+        onClick: () => {
+          setOption('show_notifications', true);
+          createNotification({
+            type: 'basic',
+            iconUrl: pontoonLogo,
+            title: 'Notification from Pontoon',
+            message:
+              'Similar notifications will appear if there is something new in Pontoon. Click them to open related project in Pontoon.',
+          });
+        },
+      },
+    },
+    {
+      title: 'Add-on settings',
+      imageSrc: settingsImage,
+      text: (
+        <>
+          Pontoon button, notifications, ... all these features are{' '}
+          <strong>configurable</strong>.
+        </>
+      ),
+      button: {
+        text: 'Open Pontoon Add-on settings',
+        onClick: () => openOptions(),
+      },
+    },
+    ...(supportsAddressBar()
+      ? [addressBarTile, contextButtonsTile]
+      : [contextButtonsTile, contextMenuTile]),
+    {
+      title: 'Feedback and more',
+      imageSrc: feedbackImage,
+      text: (
+        <>
+          This add-on won&apos;t exist and improve without you - Mozilla
+          localizers. Please <strong>share your feedback</strong> and request
+          new features.
+        </>
+      ),
+      button: {
+        text: 'Check the wiki',
+        onClick: () => openNewTab(pontoonAddonWiki()),
+      },
+    },
+  ];
 
   return (
     <>
