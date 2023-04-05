7e55a432ed2c1a3e3aec355726219dd9c45570f2
Michal Stanke <michal@stanke.cz>
2023-04-05 10:49:39 +0200

    build: add type-checked ESLint config

---
+++ configs/.eslintrc.ts
@@ -0,0 +1,91 @@
+import path from 'path';
+import type { ESLint } from 'eslint';
+
+const config: ESLint.ConfigData = {
+  extends: [
+    'eslint:recommended',
+    'plugin:compat/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:jest/recommended',
+    'plugin:jest/style',
+    'plugin:testing-library/dom',
+    'plugin:testing-library/react',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'prettier',
+    'plugin:prettier/recommended', // must be the last one
+  ],
+  ignorePatterns: [
+    '**/generated/**',
+  ],
+  rules: {
+    'import/order': [
+      'error',
+      {
+        groups: [ 'builtin', 'external', 'internal', 'parent', 'sibling', 'index' ],
+        'newlines-between': 'always',
+      },
+    ],
+    'import/newline-after-import': [
+      'error',
+      {
+        count: 1,
+      },
+    ],
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+      },
+    ],
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      {
+        prefer: 'type-imports',
+      },
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'all',
+        tabWidth: 2,
+      },
+    ],
+    'react/prop-types': 'off',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/parsers': {
+      '@typescript-eslint/parser': [ '.ts', '.tsx' ],
+    },
+    'import/resolver': {
+      webpack: {
+        // eslint-import-resolver-webpack only works with synchronous Webpack configs
+        config: path.resolve(__dirname, 'webpack.common.config.ts'),
+      },
+    },
+  },
+  env: {
+    webextensions: true,
+    browser: true,
+    jest: true,
+    es2020: true,
+  },
+};
+
+export default config;
